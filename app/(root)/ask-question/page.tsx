299513bdcaaa238392db63409c720a43cb9aa84f
R-Sidhartha <rsidhartha20@iitk.ac.in>
2025-03-02 23:47:04 +0530

    few fixes

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -2,7 +2,7 @@ import Question from '@/components/forms/Question'
 import { getUserById } from '@/lib/actions/user.action';
 import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
-import React from 'react'
+import React, { Suspense } from 'react'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -23,7 +23,10 @@ const Page = async () => {
         <div>
             <h1 className='h1-bold text-dark100_light900'>Ask a question</h1>
             <div className='mt-9'>
-                <Question mongoUserId={JSON.stringify(mongoUser?._id)} />
+                <Suspense fallback={<p>Loading Question...</p>}>
+                    <Question mongoUserId={JSON.stringify(mongoUser?._id)} />
+                </Suspense>
+
             </div>
         </div>
     )
